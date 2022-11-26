1cd05b4ee9dd082359545179b0e05fd19d529bb5
k2ying <k2ying@uwaterloo.ca>
2022-11-26 17:51:52 -0500

    Added pieces.h

---
+++ piece.h
@@ -0,0 +1,49 @@
+class Piece{
+	char color;
+	int col, row;
+	std::string name;
+
+}
+
+class Knight : public Piece{
+	public:
+		Knight(char color);
+}
+
+class Bishop : public Piece{
+	public:
+		Bishop(char color);
+}
+
+class Rook : public Piece{
+	bool castled;
+	public:
+		Rook(char color);
+		bool getCastle();
+		void setCastle();
+}
+
+class King : public Piece{
+	bool isChecked, castled;
+	public:
+		King(char color);
+		bool getStatusCheck();
+		bool getStatusCastle();
+		void setCheck();
+		void setCastle();
+}
+
+class Queen : public Piece{
+	public:
+		Queen(char color);
+}
+
+class Pawn : public Piece{
+	bool firstStep;
+	bool canPromote;
+	public:
+		Pawn(char color);
+		bool getFirstStep();
+		void setFirstStep();
+		void setCanPromote();
+}
