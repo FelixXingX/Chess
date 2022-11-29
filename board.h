e53f72cdfa7bbed2c0fd65bae520ca8450d16ca1
FelixXingX <xingfelix@hotmail.com>
2022-11-29 17:54:00 -0500

    functions in board.cc
    checked()
    checkmate()
    possiblemoves2()
    isLegalMove()
    move()

--- board.h
+++ board.h
@@ -16,7 +16,7 @@ class Board : public Subject{
 		void printMoves();
 		void move(std::string from, std::string to, std::string turn);
         	//std::vector<Vec> possibleMoves(Piece piece, int row, int col, Board board);
-        	bool canMove(int fromX, int fromY, int toX, int toY, std::string turn, Board board);
+        	bool isLegalMove(int fromX, int fromY, int toX, int toY, std::string turn, Board board, bool inCheck);
         	bool addPiece(int row, int col, std::shared_ptr<Piece>);
 		std::string getState(int row, int col) const override;
 		int getSquare(int row, int col);
@@ -24,5 +24,7 @@ class Board : public Subject{
 		std::vector<Vec> possibleMoves(Piece piece, int row, int col, Board board);
 		void render();
 		Board(std::vector<std::vector<Squares>> board, bool whiteCheck,bool blackCheck,bool whiteCheckmate,bool blackCheckmate, bool stalemate);
+		bool checked();
+		bool checkmate();
 };
 #endif
