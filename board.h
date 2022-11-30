52ef82e57bedcdc4aca1050f114d440d658fd4e8
KelvYing <113941617+KelvYing@users.noreply.github.com>
2022-11-29 20:15:35 -0500

    Add piece now works.

--- board.h
+++ board.h
@@ -15,16 +15,17 @@ class Board : public Subject{
 		void removePiece(int row, int col);
 		void printMoves();
 		void move(std::string from, std::string to, std::string turn);
-        	//std::vector<Vec> possibleMoves(Piece piece, int row, int col, Board board);
+        	//std::vector<Vec> possibleMoves(shared_ptr<Piece> piece, int row, int col, Board board);
         bool isLegalMove(int fromX, int fromY, int toX, int toY, std::string turn, Board board, bool inCheck);
         void addPiece(int row, int col, char name);
 		char getState(int row, int col) const override;
 		int getSquare(int row, int col);
 		std::shared_ptr<Piece> getPiece(int row, int col);
-		std::vector<Vec> possibleMoves(Piece piece, int row, int col, Board board);
+		std::vector <Vec> possibleMoves(std::shared_ptr<Piece> piece, int row, int col, Board board);
 		void render();
 		Board(std::vector<std::vector<Squares>> board, bool whiteCheck,bool blackCheck,bool whiteCheckmate,bool blackCheckmate, bool stalemate);
 		bool checked();
 		bool checkmate();
+		std::vector<Vec> possibleMoves2(std::shared_ptr<Piece> piece, int row, int col, Board board, std::vector<Vec> moves);
 };
 #endif
