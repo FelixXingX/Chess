37075326568aa0ed5fa7a49161597862bbc8aae2
KelvYing <113941617+KelvYing@users.noreply.github.com>
2022-12-06 16:39:54 -0500

    Update piece.cc

--- piece.cc
+++ piece.cc
@@ -29,9 +29,9 @@ Rook::Rook(string color, char name, int col, int row) : Piece{color, name, col, 
 Pawn::Pawn(string color, char name, int col, int row) : Piece{color, name, col, row, moved} {}
 
 //clone method to return copys
-shared_ptr<Piece> Knight::clone(){return make_shared<Knight>(color, name, col, row);}
-shared_ptr<Piece> King::clone(){return make_shared<King>(color, name, col, row);}
-shared_ptr<Piece> Bishop::clone(){return make_shared<Bishop>(color, name, col, row);}
-shared_ptr<Piece> Queen::clone(){return make_shared<Queen>(color, name, col, row);}
-shared_ptr<Piece> Rook::clone(){return make_shared<Rook>(color, name, col, row);}
-shared_ptr<Piece> Pawn::clone(){return make_shared<Pawn>(color, name, col, row);}
+shared_ptr<Piece> Knight::clone(){return make_shared<Knight>(color, name, col, row, moved);}
+shared_ptr<Piece> King::clone(){return make_shared<King>(color, name, col, row, moved);}
+shared_ptr<Piece> Bishop::clone(){return make_shared<Bishop>(color, name, col, row, moved);}
+shared_ptr<Piece> Queen::clone(){return make_shared<Queen>(color, name, col, row, moved);}
+shared_ptr<Piece> Rook::clone(){return make_shared<Rook>(color, name, col, row, moved);}
+shared_ptr<Piece> Pawn::clone(){return make_shared<Pawn>(color, name, col, row, moved);}
